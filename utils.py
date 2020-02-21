185ed2da3d08050f30f60f823846a1f8da4b5a62
Johnny Wan <johnnywon@g.ucla.edu>
2020-02-21 13:23:54 -0800

    add OsMismatch excpetion to utils

--- utils.py
+++ utils.py
@@ -1,1 +1,11 @@
 # some shared data structure
+
+class OsError(Exception):
+    pass
+
+class OsMismatch(Exception):
+    '''
+    exception prepared to throw when OS-specific modules are loaded on wrong system
+    e.g. loading Msvcrt_Input in linux
+    '''
+    pass
