8bd57c2ac5eeb4158737e2f69c65b1306426a72c
Johnny Wan <johnnywon@g.ucla.edu>
2020-02-22 14:24:42 -0800

    a working c python module example

--- Makefile
+++ Makefile
@@ -1,5 +1,12 @@
 cc=gcc
 flags=-Wall -Wextra -fPIC -g -shared
+pyflags=-I/usr/include/python3.7 -I/usr/lib/python3.7/config
 
 Stdin_Input: Stdin_Input.c 
-	$(cc) Stdin_Input.c -o example.so $(flags) -I/usr/include/python2.6 -I/usr/lib/python2.6/config
+	$(cc) Stdin_Input.c -o Stdin_Input.so $(flags) $(pyflags)
+
+example: PyExample.c
+	$(cc) PyExample.c -o example.so $(flags) $(pyflags)
+
+clean:
+	rm -f *.so
---
+++ PyExample.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <python3.6m/Python.h>
+
+int fact(int n)
+{
+    if (n <= 1)
+        return 1;
+    else
+        return n * fact(n - 1);
+}
+
+PyObject *wrap_fact(PyObject *self, PyObject *args)
+{
+    int n, result;
+    if (!PyArg_ParseTuple(args, "i:fact", &n))
+    return NULL;
+    result = fact(n);
+    return Py_BuildValue("i", result);
+}
+
+static PyMethodDef exampleMethods[] =
+{
+    {"fact", wrap_fact, METH_VARARGS, "Caculate N!"},
+    {NULL, NULL}
+};
+
+static PyModuleDef example =
+{
+    PyModuleDef_HEAD_INIT,
+    "example", /* name of module */
+    "",          /* module documentation, may be NULL */
+    -1,          /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
+    exampleMethods
+};
+
+PyMODINIT_FUNC PyInit_example(void)
+{
+    return PyModule_Create(&example);
+}
