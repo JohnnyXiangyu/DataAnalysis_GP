1f7e4b50db3df118541b0c7a1e4f74d30482b565
Johnny Wan <johnnywon@g.ucla.edu>
2020-02-26 14:09:15 -0800

    started to bring back logger module

--- Logger.py
+++ Logger.py
@@ -1,27 +1,50 @@
 import os
+import sys
 print('Loading logger module ...')
 
 
-class Logger:
+class simpleLogger:
+    # TODO: all open/close functions don't check failure yet
     '''
     logger object
     '''
 
-    def __init__(self, sensorType):
-        """file open"""
-        fileName = input('| Enter sequence of test: ')
-        self.m_file = open('Data_Analysis_win64\\test_data\\' + sensorType +
-                           'test' + fileName + '.csv', 'w')
-        os.system('cls')
-        print('Out file is open.')
+    def __init__(self, msg, ind, dire=None):
+        """
+        initialize logger: open/create file
+        dire: path to the file
+        msg: any information besides test count
+        index: numbering, type, anything related to defining an entry in a series of tests
+        """
+        if dire == None:
+            dire = os.getcwd() + '/'
+        self.m_file = open(dire + msg +
+                           'test' + ind + '.csv', 'w')
 
-    def printFile(self, data_list):
-        '''print a list of data, includng flags, into the out file'''
+    def printFile(self, data_list, separator=','):
+        '''
+        print a line of record as-is, not extra formatting
+        data_list: a list (or any iteratable) object
+        separator: default ',', deliminator in record
+        '''
         for data in data_list:
             self.m_file.write(str(data))
-            self.m_file.write(', ')
+            self.m_file.write(separator)
         self.m_file.write('\n')
+
+    def switchFile(self, msg, ind, dire=None):
+        """
+        close previous log file, then open a new one
+        dire: path to the file
+        msg: any information besides test count
+        index: numbering, type, anything related to defining an entry in a series of tests
+        """
+        self.m_file.close()
+        if dire == None:
+            dire = os.getcwd() + '/'
+        self.m_file = open(dire + msg +
+                           'test' + ind + '.csv', 'w')
+
     
     def close(self):
         self.m_file.close()
-        print('Out file is closed.')
